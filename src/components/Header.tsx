daa19b3a3628ecc48735947ef129cd3f91b161c2
gepog <gepoguo@gmail.com>
2025-06-29 00:14:54 +0200

    Update header navigation items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -55,7 +55,7 @@ export const Header: React.FC<HeaderProps> = ({
     }
   };
 
-  const navItems = ['Home', 'TV Shows', 'Movies', 'New & Popular', 'My List'];
+  const navItems = ['Home', 'Popular', 'My List'];
 
   return (
     <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
